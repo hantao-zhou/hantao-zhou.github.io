a730f96c7bbe39772cff0f5173cb495f869c5239
hantao <hantaozhouted@gmail.com>
2025-08-20 16:44:57 +0800

    new stuff here

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,md,mdx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
