94f9c29edc5d9561c639e18fc5b4a63c18dc45cf
hantao <hantaozhouted@gmail.com>
2025-08-20 13:26:34 +0800

    all removed

--- src/app/cv/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main>
-      <h1 className="mb-4 text-xl">CV</h1>
-      <p><a href="#">Download CV (coming soon)</a></p>
-    </main>
-  )
-}
--- src/app/talks/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main>
-      <h1 className="mb-4 text-xl">Talks</h1>
-      <p>Talks will be listed here.</p>
-    </main>
-  )
-}
