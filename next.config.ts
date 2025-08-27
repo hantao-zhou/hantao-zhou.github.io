39b2092d216c335cfc5d5da9df2964fe5f152a7b
hansgutmann <hantaozhouted@gmail.com>
2025-08-27 19:58:35 +0800

    deletes

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  images: { unoptimized: true } // needed when exporting statically
-};
-
-export default nextConfig;
